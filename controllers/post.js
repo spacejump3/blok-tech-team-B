0eb45bbc1e8a2df23d9ede16e2248635f246cb33
jkhvacmd209 <joppe.koops@hva.nl>
2023-03-28 15:32:27 +0200

    Bereid object voor om in te voegen in de database

--- controllers/post.js
+++ controllers/post.js
@@ -2,6 +2,20 @@ exports.postForm = (req, res) => {
     res.render('postform')
 }
 
-exports.submit = (req, res) => {
-    console.log(req.files)
+exports.submit = async (req, res) => {
+    try {
+        let newPet = {
+            _id: req.files[0].destination.split('/').at(-1),
+            age: req.body.age,
+            name: req.body.name,
+            species: req.body.species,
+            trait: req.body.trait,
+            liked: false,
+            comments: []
+        }
+
+        console.log(newPet)
+    } catch (err) {
+        console.log(err.stack)
+    }
 }
