c280a099be4113a5ee99a2ac4657e52069c4c383
lucaoudejans <luca.oudejans@hva.nl>
2023-03-28 15:05:42 +0200

    added register feature

--- controllers/register.js
+++ controllers/register.js
@@ -1,29 +1,34 @@
 const {
     Db
-  } = require("mongodb")
-  
-  exports.registrerenPagina = (req, res) => {
+} = require("mongodb")
+
+exports.registrerenPagina = (req, res) => {
     res.render('register.ejs')
-  }
-  
-  exports.accountAanmaken = async (req, res) => {
+}
+
+exports.accountAanmaken = async (req, res) => {
     try {
-      const email_filled = req.body.email.toLowerCase()
-      const password_filled = req.body.wachtwoord.toLowerCase()
-  
-      const userValidation = await req.app.get('database').collection('users').findOne({
-        email: email_filled,
-        password: password_filled
-      })
-  
-      console.log(userValidation)
-  
-      if (!userValidation) {
-        res.send('geen gebruiker gevonden')
-      } else {
-        res.send('ingelogd...')
-      }
+        const username_filled = req.body.username.toLowerCase()
+        const email_filled = req.body.email.toLowerCase()
+        const password_filled = req.body.wachtwoord.toLowerCase()
+
+        // controleer of de gebruiker al bestaat in de database 
+        const userValidation = await req.app.get('database').collection('users').findOne({
+            email: email_filled
+        })
+        if (userValidation) {
+            res.send('gebruiker bestaat al')
+        }
+
+        // als de gebruiker niet bestaat, voeg dan de nieuwe gebruiker toe aan de database 
+        const newUser = {
+            username: username_filled,
+            email: email_filled,
+            password: password_filled
+        }
+        await req.app.get('database').collection('users').insertOne(newUser)
+        res.send('gebruiker geregistreerd')
     } catch (err) {
-      console.log(err)
+        console.log(err)
     }
-  }
+}
