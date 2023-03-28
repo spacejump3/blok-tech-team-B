8248308e1dec2c0d39f63a15f9f71a4c0ead6921
jkhvacmd209 <joppe.koops@hva.nl>
2023-03-28 12:39:00 +0200

    Installeer en require multer

--- controllers/post.js
+++ controllers/post.js
@@ -1,3 +1,22 @@
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'static/upload/')
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname)
+    },
+})
+
+const upload = multer({
+    storage: storage
+})
+
 exports.postForm = (req, res) => {
     res.render('postform')
+}
+
+exports.submit = (req, res) => {
+
 }
--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+	destination: (req, file, callback) => {
+		callback(null, 'static/upload/')
+	},
+	filename: (req, file, callback) => {
+		callback(null, file.originalname)
+	},
+})
+
+const upload = multer({
+	storage: storage
+})
+
 const postController = require('../controllers/post.js')
 
 router.get('/', postController.postForm)
+
+router.post('/submit', upload.array('images'), postController.submit)
 
 module.exports = router
