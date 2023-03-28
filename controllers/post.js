34ab8244424144065599165cbf83181d6edde5d2
jkhvacmd209 <joppe.koops@hva.nl>
2023-03-28 14:48:04 +0200

    Plaats geüploade afbeeldingen met multer in een folder met een mongoDB objectID als naam
    Door per dier een folder te maken blijven de afbeeldingen georganiseerd. Door die folder een objectID als naam te geven is het makkelijker te koppelen aan een bestand in de database met diezelfde objectID

--- controllers/post.js
+++ controllers/post.js
@@ -1,22 +1,7 @@
-const multer = require('multer')
-
-const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, 'static/upload/')
-    },
-    filename: (req, file, callback) => {
-        callback(null, file.originalname)
-    },
-})
-
-const upload = multer({
-    storage: storage
-})
-
 exports.postForm = (req, res) => {
     res.render('postform')
 }
 
 exports.submit = (req, res) => {
-
+    console.log(req.files)
 }
