ac61f5062814043c1fb21dc4d6617001ac5f5b56
jkhvacmd209 <joppe.koops@hva.nl>
2023-03-30 15:20:12 +0200

    Verwijder verkeerd gemergde code

--- controllers/profile.js
+++ controllers/profile.js
@@ -4,16 +4,13 @@ const {
 const ObjectId = require('mongodb').ObjectId
 const session = require('express-session')
 
-exports.mijnProfiel = (req, res) => {
-    const user = req.session.user
 
-    exports.profielPagina = async (req, res) => {
-        userid = req.session.userid
+exports.profielPagina = async (req, res) => {
+    userid = req.session.userid
 
-        const data = await req.app.get('database').collection('users').findOne({ _id: new ObjectId(userid) })
+    const data = await req.app.get('database').collection('users').findOne({ _id: new ObjectId(userid) })
 
-        res.render('profile.ejs', {
-            data: data
-        })
-    }
+    res.render('profile.ejs', {
+        data: data
+    })
 }
