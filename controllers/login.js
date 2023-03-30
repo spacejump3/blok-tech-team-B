45073e6d3402867663cb5758f2e387972d25f4b9
LisaxLF <Lisafokkinga@gmail.com>
2023-03-30 13:53:43 +0200

    test

--- controllers/login.js
+++ controllers/login.js
@@ -12,20 +12,22 @@ exports.inloggen = async (req, res) => {
     const email_filled = req.body.email.toLowerCase()
     const password_filled = req.body.wachtwoord.toLowerCase()
 
-    const user = await req.app.get('database').collection('users').findOne({
+    const userValidation = await req.app.get('database').collection('users').findOne({
       email: email_filled,
       password: password_filled,
     })
+    
+    const user = await req.app.get('database').collection('users').findOne({
+      _id: req.body.id
+    })
 
-    // console.log(user)
-
-    if (user) {
+    if (userValidation) {
       req.session.user = user
       // de gebruiker is gevonden de sessie word nu gestart voor de gebruiker
       console.log(req.session.user)
 
       // de gebruiker word nu gestuurd naar mijn profiel route
-      res.redirect('/profile', {username: req.session.user.username})
+      res.redirect('/profile')
     } else {
       res.redirect('/login')
     }
