db97b20ddf4cd0c143242784e869977edd1c4c2c
thimogagliano <94388546+thimogagliano@users.noreply.github.com>
2023-04-05 13:26:11 +0200

    Pets can now get liked/unliked with _id instead of the name

--- controllers/liked.js
+++ controllers/liked.js
@@ -1,12 +1,20 @@
-exports.like = async (req, res) => {
-    const favoritePet = req.body.favorite;
-    console.log(favoritePet);
+const ObjectId = require('mongodb').ObjectId
 
-    const filter = { name: favoritePet };
+exports.like = async (req, res) => {
+    const favoritePetId = req.body.favorite;
+    console.log(favoritePetId);
+
+    const favoritePetName = req.body.petname
+    console.log(favoritePetName)
+
+    // const likedCheck = req.body.liked;
+    // console.log(likedCheck)
+
+    const filter = { _id: new ObjectId(favoritePetId)};
 
     const updateDocument = { $set: { liked: true} };
 
     const update = await req.app.get('database').collection('pets').updateOne(filter, updateDocument);
 
-    res.render('liked', { favoritePetLiked: "Succesfully added " + favoritePet + " to favorites!" });
+    res.render('liked', { favoritePetLiked: "Succesfully added " + favoritePetName + " to favorites!" });
 };
